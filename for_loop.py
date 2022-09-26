13dde1517df74ed082e4f83cc124df99256c78ee
tanvir07-cell <tanvir15-14402@diu.edu.bd>
2022-09-26 23:22:32 +0700

    for the dictinary

---
+++ for_loop.py
@@ -0,0 +1,22 @@
+
+
+# numbers = [1, 2, 3, 4, 5, 6]
+# sum = 0
+
+# for i in numbers:
+#     sum = sum+i
+# print(sum)
+
+
+# for in range:
+
+# for i in range(2, 10, 2):
+#     print(i)
+
+
+dic = {"Tanvir Rifat": 3.78, "Arafat": 2.45}
+student_name = "Arafat"
+
+for i in dic:
+    if i == student_name:
+        print(dic[i])
